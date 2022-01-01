b4158723a035d09aea6dca97462eb07800ee5a9e
Grigore Stefan <g_stefan@yahoo.com>
2022-01-01 21:25:31 +0200

    Update

--- source/quantum-script-extension-csv-version.cpp
+++ source/quantum-script-extension-csv-version.cpp
@@ -15,10 +15,10 @@ namespace Quantum {
 			namespace CSV {
 				namespace Version {
 
-					static const char *version_ = "2.4.0";
-					static const char *build_ = "20";
-					static const char *versionWithBuild_ = "2.4.0.20";
-					static const char *datetime_ = "2021-11-22 17:07:59";
+					static const char *version_ = "2.7.0";
+					static const char *build_ = "21";
+					static const char *versionWithBuild_ = "2.7.0.21";
+					static const char *datetime_ = "2022-01-01 21:19:39";
 
 					const char *version() {
 						return version_;
